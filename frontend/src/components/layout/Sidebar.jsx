2fbdeb04269d01f034e05164e0552a5cadeda062
Md Shoaib <mdshoaib@Mds-MacBook-Air.local>
2026-09-15 22:19:25 +0530

    Fix workspace selector on desktop

--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -145,7 +145,7 @@ const Sidebar = ({ mobileOpen, setMobileOpen }) => {
           flex h-screen w-64 shrink-0 flex-col
           bg-[#18211D] text-white
           transition-transform duration-300
-          lg:sticky lg:top-0 lg:z-auto
+          lg:sticky lg:top-0 lg:z-50
           lg:flex lg:h-screen
           lg:translate-x-0
           ${mobileOpen ? "translate-x-0" : "-translate-x-full"}
